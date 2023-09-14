3725a0d01a2df5f39d8425a670808c0a24a5dca5
DikshikaJain <dikshika.jain1702@gmail.com>
2023-09-14 20:11:46 +0000

    fixed sentinel errors

--- main.tf
+++ main.tf
@@ -35,6 +35,35 @@ resource "aws_security_group" "hashicat" {
 
   vpc_id = aws_vpc.hashicat.id
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["213.105.94.139/32"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["213.105.94.139/32"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["213.105.94.139/32"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["213.105.94.139/32"]
+    prefix_list_ids = []
+  }
+
   tags = {
     Name = "${var.prefix}-security-group"
   }
