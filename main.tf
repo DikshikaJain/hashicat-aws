92e091f054c615b3221ab98b9711cb644eb97dc0
DikshikaJain <dikshika.jain1702@gmail.com>
2023-09-14 20:00:06 +0000

    fixed sentinel errors

--- main.tf
+++ main.tf
@@ -46,21 +46,21 @@ resource "aws_security_group" "hashicat" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["172.17.0.1/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["172.17.0.1/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port       = 0
     to_port         = 0
     protocol        = "-1"
-    cidr_blocks     = ["172.17.0.1/32"]
+    cidr_blocks     = ["0.0.0.0/0"]
     prefix_list_ids = []
   }
 
--- remote_backend.tf
+++ remote_backend.tf
@@ -3,7 +3,7 @@ terraform {
     hostname = "app.terraform.io"
     organization = "dikshika-training"
     workspaces {
-      name = "hashicat-aws"
+      name = "hashicat-dev"
     }
   }
 }
